732848fcc9dc8ec57d85df4eb4bcb8348ee44ed0
snake <scarletsnakewong@gmail.com>
2021-10-04 06:14:29 +0800

    update

--- ArcadeLand/Assets/ThirdParty/art/SpareParts/Demo/Scripts/Rotate.cs
+++ ArcadeLand/Assets/ThirdParty/art/SpareParts/Demo/Scripts/Rotate.cs
@@ -2,27 +2,8 @@
 
 public class Rotate : MonoBehaviour
 {
-	public Space space = Space.Self;
-	public Vector3 speed = new  Vector3(0f, 3f, 0f);
+	public float speed = 3f;
 
-	private float smoothing;
-	private Vector3 currentUpdateSpeed;
-
-	public void ApplyRotation () { ApplyRotation ( 1f,1f,1f,1f); }
-
-	public void ApplyRotation ( 
-		float timeScale = 0f, 
-		float time=0f, 
-		float deltatime = 0f, 
-		float smoothDeltatime = 0f )
-	{
-		smoothing = smoothDeltatime == 0 
-			? Time.smoothDeltaTime : smoothDeltatime;
-
-		currentUpdateSpeed.x = speed.x * smoothing;
-		currentUpdateSpeed.y = speed.y * smoothing;
-		currentUpdateSpeed.z = speed.z * smoothing;
-
-		transform.Rotate ( currentUpdateSpeed, space );
-	}
+	public void Update ( )
+		=> transform.Rotate( 0f, speed * Time.smoothDeltaTime, 0f);
 }
